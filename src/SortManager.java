9f90ca5af8db8536c4dcdd1c21d947dec51c97ae
Renan Liberato Sant'Ana <renan.libsantana@gmail.com>
2016-05-11 00:09:40 -0300

    Core adequation to Google's code guide and attributes visibility changes

--- src/SortManager.java
+++ src/SortManager.java
@@ -26,13 +26,12 @@ public class SortManager extends Sort
     /**
      * Objeto da classe FileManager utilizado na manipulação de arquivos.
      */
-    FileManager fileManager;
+    private FileManager fileManager;
 
     /**
      * Método construtor responsável por instanciar uma dependência, a classe FileManager.
      */
-    public SortManager()
-    {
+    public SortManager() {
         this.fileManager = new FileManager();
     }
 
@@ -41,44 +40,55 @@ public class SortManager extends Sort
      * @param file Caminho do arquivo desordenado.
      * @param filePath Caminho da pasta onde se encontra o arquivo desordenado.
      */
-    public void start(String file, String filePath)
-    {
+    public void start(String file, String filePath) {
+
         fileManager.setFile(file, filePath);
         String methodToCompare = method[2];
+
         if (methodToCompare.compareTo("double") == 0) {
             this.doubleList = fileManager.readDouble();
+
             sortDouble();
+
             fileManager.recordDouble(this.doubleList);
+
         } else if (methodToCompare.compareTo("String") == 0) {
             this.stringList = fileManager.readString();
+
             sortString();
+
             fileManager.recordString(stringList);
         }
+
         fileManager.recordLog(this.dataInicial, this.dataFinal);
     }
 
     /**
      * Método responsável por encaminhar o vetor  de double desordenado ao método de ordenação selecionado pelo usuário.
      */
-    public void sortDouble()
-    {
+    private void sortDouble() {
         dataInicial = System.currentTimeMillis();
 
         switch (this.method[0]) {
             case "Selection":
                 this.doubleList = selection(this.doubleList);
+
                 break;
             case "Bubble":
                 this.doubleList = bubble(this.doubleList);
+
                 break;
             case "Insertion":
                 this.doubleList = insertion(this.doubleList);
+
                 break;
             case "QuickSort":
                 this.doubleList = quickSort(this.doubleList, 0, this.doubleList.length-1);
+
                 break;
             case "ShellSort":
                 this.doubleList = shellSort(this.doubleList);
+
                 break;
         }
         dataFinal = System.currentTimeMillis();
@@ -87,25 +97,29 @@ public class SortManager extends Sort
     /**
      * Método responsável por encaminhar o vetor  de Strings desordenado ao método de ordenação selecionado pelo usuário.
      */
-    public void sortString()
-    {
+    private void sortString() {
         dataInicial = System.currentTimeMillis();
 
         switch (this.method[0]) {
             case "Selection":
                 this.stringList = selection(this.stringList);
+
                 break;
             case "Bubble":
                 this.stringList = bubble(this.stringList);
+
                 break;
             case "Insertion":
                 this.stringList = insertion(this.stringList);
+
                 break;
             case "QuickSort":
                 this.stringList = quickSort(this.stringList, 0, this.stringList.length-1);
+
                 break;
             case "ShellSort":
                 this.stringList = shellSort(this.stringList);
+
                 break;
         }
         dataFinal = System.currentTimeMillis();
@@ -115,8 +129,8 @@ public class SortManager extends Sort
      * Getter de dataInicial
      * @return retorna um valor em milisegundos
      */
-    public long getDataInicial()
-    {
+    public long getDataInicial() {
+
         return dataInicial;
     }
 
@@ -124,8 +138,7 @@ public class SortManager extends Sort
      * Setter de dataInicial
      * @param dataInicial valor em milisegundos a ser armazenado
      */
-    public void setDataInicial(long dataInicial)
-    {
+    public void setDataInicial(long dataInicial) {
         this.dataInicial = dataInicial;
     }
 
@@ -134,6 +147,7 @@ public class SortManager extends Sort
      * @return retorna um valor em milisegundos
      */
     public long getDataFinal() {
+
         return dataFinal;
     }
 
@@ -141,8 +155,7 @@ public class SortManager extends Sort
      * Setter de dataFinal
      * @param dataFinal valor em milisegundos a ser armazenado
      */
-    public void setDataFinal(long dataFinal)
-    {
+    public void setDataFinal(long dataFinal) {
         this.dataFinal = dataFinal;
     }
 
@@ -150,8 +163,8 @@ public class SortManager extends Sort
      * Getter do vetor de Strings gerado do seletor de métodos da interface.
      * @return Retorna um vetor de comprimento 3.
      */
-    public String[] getMethod()
-    {
+    public String[] getMethod() {
+
         return method;
     }
 
@@ -159,9 +172,9 @@ public class SortManager extends Sort
      * Setter do vetor de Strings gerado do seletor de métodos da interface.
      * @param method Vetor de Strings de comprimento 3.
      */
-    public void setMethod(String[] method)
-    {
+    public void setMethod(String[] method) {
         this.method = method;
+        
         fileManager.setMethod(method);
     }
 }
